86a08574ef63b7ca955d452fd65f084966595e61
Paul Steele <paul@paul-steele.com>
2025-11-25 21:38:04 -0500

    clean up parsing

--- FerngillSimpleEconomy/models/contentPacks/BaseContentPackItem.cs
+++ FerngillSimpleEconomy/models/contentPacks/BaseContentPackItem.cs
@@ -4,9 +4,5 @@ using fse.core.helpers;
 namespace fse.core.models.contentPacks;
 
 [JsonConverter(typeof(ContentPackItemJsonConverter))]
-public abstract class BaseContentPackItem
-{
-	[JsonPropertyName("action")]
-	public string Action { get; set; } = string.Empty;
-}
+public abstract class BaseContentPackItem;
 
