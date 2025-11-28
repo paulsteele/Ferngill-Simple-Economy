815b82f1fa89db37d1bba3619819143e8839cf0a
Paul Steele <paul@paul-steele.com>
2025-11-27 20:56:59 -0500

    update api reference

--- FerngillSimpleEconomy/api/IFerngillSimpleEconomyApi.cs
+++ FerngillSimpleEconomy/api/IFerngillSimpleEconomyApi.cs
@@ -9,20 +9,20 @@ namespace fse.core.api;
 public interface IFerngillSimpleEconomyApi
 {
 	/// <summary>
-	/// Gets whether the economy has been loaded. No actions can be taken if the economy is not loaded.
+	/// Gets whether the economy has been loaded. No actions can be taken if the economy is not loaded. Occurs during GameLoop.SaveLoaded.
 	/// </summary>
 	/// <returns>True if the economy has been loaded, false otherwise.</returns>
 	bool IsLoaded();
 
 	/// <summary>
-	/// Adjusts the supply value for the specified object. Will update supply for all players in multiplayer.
+	/// Adjusts the supply value for the specified object. In general, the amount should be positive if the player sold an item. The value won't go outside the boundaries set in the config. Only one farmer should invoke this during multiplayer.
 	/// </summary>
 	/// <param name="obj">The object to adjust supply for.</param>
 	/// <param name="amount">The amount to adjust supply by (can be negative).</param>
 	void AdjustSupply(Object obj, int amount);
 
 	/// <summary>
-	/// Adjusts the daily delta value for the specified object. Will update daily delta for all players in multiplayer.
+	/// Adjusts the daily delta value for the specified object. Only one farmer should invoke this during multiplayer.
 	/// </summary>
 	/// <param name="obj">The object to adjust daily delta for.</param>
 	/// <param name="amount">The amount to adjust daily delta by (can be negative).</param>
@@ -43,7 +43,7 @@ public interface IFerngillSimpleEconomyApi
 	int? GetDelta(Object obj);
 
 	/// <summary>
-	/// Checks if the specified object is tracked in the economy.
+	/// Checks if the specified object is tracked in the economy. It is recommended to call other methods without checking this first as they will handle the item not existing in the economy gracefully. 
 	/// </summary>
 	/// <param name="obj">The object to check.</param>
 	/// <returns>True if the object is in the economy, false otherwise.</returns>
